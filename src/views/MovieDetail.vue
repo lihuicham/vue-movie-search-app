ee5a0d18dc6864600e3a3db3528dd8214b18cfc3
Li Hui Cham <lihuicham@gmail.com>
2023-02-03 00:46:32 +0800

    feat/MovieDetail completed

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,13 +1,59 @@
 <template>
     <div class="movie-detail">
-        Movie Details {{ $route.params.id }}
+        <h2>{{ movie.Title }}</h2>
+        <p>{{ movie.Year }}</p>
+        <img :src="movie.Poster" :alt="movie.Title + ' poster'"/>
+        <p>{{ movie.Plot }}</p>
     </div>
 </template>
 <script>
+import { ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router';
+import env from '@/env';
+
 export default {
-    
+    setup() {
+        const movie = ref({});
+        const route = useRoute();
+
+        onBeforeMount(() => {
+            fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+                .then(response => response.json())
+                .then(data => {
+                    movie.value = data  // returns a Movie object
+
+                })
+        });
+
+        return {
+            movie,
+            route
+        }
+    }   
 }
 </script>
-<style lang="">
+<style lang="scss">
     
+    .movie-detail {
+        padding: 16px;
+
+        h2 {
+            color: rgb(18, 100, 126);
+            font-size: 28px;
+            font-weight: 600;
+            margin-bottom: 16px;
+        }
+
+        img {
+            display: block;
+            max-width: 200px;
+            margin-bottom: 16px;
+        }
+
+        p {
+            color: rgb(142, 73, 9);
+            font-size: 18px;
+            line-height: 1.4;
+        }
+    }
 </style>
